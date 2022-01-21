f5c8273ba95f0c8c209ef923463ad6b5895020cf
Taylor <tbuchhei@gmail.com>
2022-01-20 20:28:32 -0800

    End of section 4, redux and typescript

---
+++ redux-ts/src/state/action-creators/index.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionType } from '../action-types';
+import { Action } from '../actions/';
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
+
+    try {
+      const { data } = await axios.get('https://api.npms.io/v2/search?', {
+        params: {
+          q: term,
+        },
+      });
+
+      console.log(data);
+
+      const names = data.results.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (err: any) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
