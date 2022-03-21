875eb733f8708dea4a1bb345d5ce5652ec58210b
Taylor <tbuchhei@gmail.com>
2022-03-21 14:39:55 -0700

    End of Section 18, Binding React with Redux

---
+++ jbook/src/components/cell-list.tsx
@@ -0,0 +1,28 @@
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
+import { Fragment } from 'react';
+
+const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells }) =>
+    cells?.order.map((id) => cells.data[id])
+  );
+
+  const renderedCells = cells?.map((cell) => {
+    return (
+      <Fragment key={cell.id}>
+        <CellListItem cell={cell} />
+        <AddCell previousCellId={cell.id} />
+      </Fragment>
+    );
+  });
+
+  return (
+    <div>
+      <AddCell forceVisible={cells?.length === 0} previousCellId={''} />
+      {renderedCells}
+    </div>
+  );
+};
+
+export default CellList;
--- jbook/src/state/reducers/cellsReducer.ts
+++ jbook/src/state/reducers/cellsReducer.ts
@@ -30,14 +30,20 @@ const reducer = produce(
         return state;
       case ActionType.DELETE_CELL:
         delete state.data[action.payload];
-        state.order.filter((id) => id !== action.payload);
+        // state.order.filter((id) => id !== action.payload);
+
+        const indexToDelete = state.order.findIndex(
+          (id) => id === action.payload
+        );
+        state.order.splice(indexToDelete, 1);
+
         return state;
       case ActionType.MOVE_CELL:
         const { direction } = action.payload;
         const index = state.order.findIndex((id) => id === action.payload.id);
         const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
-        if (targetIndex < 0 || targetIndex > state.order.length) {
+        if (targetIndex < 0 || targetIndex >= state.order.length) {
           return state;
         }
 
@@ -45,7 +51,7 @@ const reducer = produce(
         state.order[targetIndex] = action.payload.id;
 
         return state;
-      case ActionType.INSERT_CELL_BEFORE:
+      case ActionType.INSERT_CELL_AFTER:
         const cell: Cell = {
           content: '',
           type: action.payload.type,
@@ -59,9 +65,9 @@ const reducer = produce(
         );
 
         if (foundIndex < 0) {
-          state.order.push(cell.id);
+          state.order.unshift(cell.id);
         } else {
-          state.order.splice(foundIndex, 0, cell.id);
+          state.order.splice(foundIndex + 1, 0, cell.id);
         }
 
         return state;
